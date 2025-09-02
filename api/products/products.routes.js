9ab7d91421f36d4bca79aabc16786eaa249740f4
Aqil0709 <aqil.jamadar09@gmail.com>
2025-09-02 12:52:04 +0530

    Initial commit for the backend API

--- api/products/products.routes.js
+++ api/products/products.routes.js
@@ -20,8 +20,8 @@ router.get('/:productId', getProductById);
 router.get('/:productId/reviews', getProductReviews);
 
 // Admin Routes
-router.post('/add', authenticate, authorizeAdmin, upload.array('images', 5), addProduct);
-router.put('/update/:productId', authenticate, authorizeAdmin, upload.array('images', 5), updateProduct);
+router.post('/add', authenticate, authorizeAdmin, upload.array('images', 15), addProduct);
+router.put('/update/:productId', authenticate, authorizeAdmin, upload.array('images', 15), updateProduct);
 router.delete('/delete/:productId', authenticate, authorizeAdmin, deleteProduct);
 router.put('/stock/update/:productId', authenticate, authorizeAdmin, updateProductStock);
 router.put('/status/:productId', authenticate, authorizeAdmin, updateProductStatus);
